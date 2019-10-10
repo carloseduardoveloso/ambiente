0155cc9b8f5d2ea90d41b84614a3e36bed8a4d7e
Carlos Eduardo Veloso <caduveloso@gmail.com>
2019-10-10 01:22:55 -0300

    adicionado a configuracao das chaves e playbook

--- configs/install_ansible.sh
+++ configs/install_ansible.sh
@@ -5,5 +5,17 @@ apt upgrade -y
 apt install software-properties-common
 apt-add-repository --yes --update ppa:ansible/ansible
 apt install ansible -y
-#ansible all -i /vagrant/ansible/hosts -m ping
 echo 'Ansible instalado'
+
+echo 'Copiando arquivos hosts e playbook'
+cp /vagrant/ansible/hosts /home/vagrant/hosts
+cp /vagrant/ansible/mysql57.playbook.yml /home/vagrant/mysql57.playbook.yml
+echo 'Copiando chave ssh'
+cp /vagrant/ssh_keys/id_rsa /home/vagrant/
+echo 'Editando permissões das chaves'
+chmod 600 /home/vagrant/id_rsa
+chown vagrant:vagrant /home/vagrant/id_rsa
+echo 'Verificando acesso as maquinas via ansible'
+ansible all -i hosts -m ping
+echo 'Executando o playbook do mysql57'
+ansible-playbook -i hosts mysql57.playbook.yml
